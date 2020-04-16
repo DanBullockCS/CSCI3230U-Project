87b858c0db82de57bba1cb41d75a00ce91106163
alexwaddell <alexander.waddell1@ontariotechu.net>
2020-04-16 19:35:27 -0400

    Adding notifier groups, adjusting notifier modal

--- frontend/src/views/CreateNotifierGroup.vue
+++ frontend/src/views/CreateNotifierGroup.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="notifier">
-    <v-btn v-on:click="modalToggle()">Add Notifier</v-btn>
+    <v-btn v-on:click="modalToggle()">Add Notifier Group</v-btn>
 
     <v-dialog v-model="isVisible" width="500">
       <v-card>
         <v-card-title class="headline grey">Create Notifier</v-card-title>
 
         <v-card-actions>
-          <v-text-field v-model="token" label="Token" outlined shaped clearable></v-text-field>
-        </v-card-actions>
-        <v-card-actions>
-          <v-textarea label="Custom Handler" outlined shaped clearable></v-textarea>
-        </v-card-actions>
-        <v-card-actions>
-          <v-textarea label="Extra Data" outlined shaped clearable></v-textarea>
+          <v-text-field v-model="name" label="Name" outlined shaped clearable></v-text-field>
         </v-card-actions>
 
         <v-card-actions>
