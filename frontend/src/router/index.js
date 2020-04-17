7924db3ed606cfd89e2dc3fe91cffa25fbf1bbe0
Daniel Bullock <Daniel.Bullock@ontariotechu.net>
2020-04-16 20:06:32 -0400

    Added darkmode switch back to settings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,7 +7,6 @@ import Trash from "../views/Trash.vue";
 import Analytics from "../views/Analytics.vue";
 import Login from "../views/Login.vue";
 import SignUp from "../views/SignUp.vue";
-import CreateNotifier from "../views/CreateNotifier.vue";
 
 Vue.use(VueRouter);
 
@@ -25,8 +24,7 @@ const routes = [
   {
     path: "/Settings",
     name: "Settings",
-    // component: Settings,
-    component: () => import(/* webpackChunkName: "about" */ "../views/Settings.vue")
+    component: Settings,
   },
   {
     path: "/Trash",
