3af24b0d77850e42c4ff038279ed20551481787f
alexwaddell <alexander.waddell1@ontariotechu.net>
2020-04-16 21:58:31 -0400

    Minor updates to modals

--- frontend/src/views/CreateNotifierGroup.vue
+++ frontend/src/views/CreateNotifierGroup.vue
@@ -2,7 +2,7 @@
   <div class="notifiergroup">
     <v-dialog v-model="isVisible" width="500">
       <v-card>
-        <v-card-title class="headline grey">Create Notifier</v-card-title>
+        <v-card-title class="headline grey">Create Notifier Group</v-card-title>
 
         <v-card-actions>
           <v-text-field v-model="name" label="Name" outlined shaped clearable></v-text-field>
